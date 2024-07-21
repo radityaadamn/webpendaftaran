0ed48eec27a412344f1c1a38bd24bb599821431c
Teguh Yudhaninglogo <adamkn8173@gmail.com>
2024-07-22 00:34:28 +0700

    first commit

--- Register/firebaseauth.js
+++ Register/firebaseauth.js
@@ -78,7 +78,7 @@ signIn.addEventListener('click', (event)=>{
         showMessage('login is successful', 'signInMessage');
         const user = userCredential.user;
         localStorage.setItem('loggedInUserId', user.uid);
-        window.location.href='/Main Page/index.html';
+        window.location.href='mainpage.html';
     })
     .catch((error)=>{
         const errorCode = error.code;
